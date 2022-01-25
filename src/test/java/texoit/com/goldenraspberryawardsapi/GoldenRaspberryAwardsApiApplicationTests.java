addce7a3300db6c867d05143ea6cde0573c2ca54
mc99078 <marcelo.correa@southsystem.com.br>
2022-01-24 22:36:28 -0300

    Initial commit

---
+++ src/test/java/texoit/com/goldenraspberryawardsapi/GoldenRaspberryAwardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package texoit.com.goldenraspberryawardsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldenRaspberryAwardsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
